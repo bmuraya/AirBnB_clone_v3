0909f823fb451b2f97a146f3572aaf831b380942
thorbie <jonahemmanuel54@gmail.com>
2023-10-29 10:56:58 +0100

    update engines and test engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,6 +48,31 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
+    def get(self, cls, id):
+        """
+        A method to retrieve on object and return the object name
+        based on its id or class. Return none if not found
+        """
+        if type(cls) == str:
+            cls = classes.get(cls)
+        if cls is None:
+            return None
+        for item in self.__objects.values():
+            if item.__class__== cls and item.id == id:
+                return item
+
+    def count(self, cls=None):
+        """A method that counts the number of objects in the storage.
+        Retuns the number of object in the storage matching the given class name,
+        if no name is passed, returns the count of all object in the storgae
+        """
+        if type(cls) is str:
+            cls = classes.get(cls)
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
+    
+    
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
